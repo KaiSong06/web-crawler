fbf20af4de5a2cdb42d2fc18f387062879f67239
KaiSong06 <kai.song421@gmail.com>
2025-06-08 03:29:38 -0400

    Create stack functions for DFS

--- main.go
+++ main.go
@@ -2,7 +2,37 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
+
+// Stack
+type Stack struct {
+	elements []string
+	mu       sync.Mutex
+}
+
+func (s *Stack) push(url string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.elements = append(s.elements, url)
+}
+
+func (s *Stack) pop() (string, bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if len(s.elements) == 0 {
+		return "", false
+	}
+	url := s.elements[len(s.elements)-1]
+	s.elements = s.elements[:len(s.elements)-1]
+	return url, true
+}
+
+func (s *Stack) size() int {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return len(s.elements)
+}
 
 func main() {
 	fmt.Println("Hello, World!")
